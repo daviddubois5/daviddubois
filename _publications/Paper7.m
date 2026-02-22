cbcbc769c311ca8464cb8f1e86aa6cbf4a500d6e
David Dubois <david.dubois.phd@gmail.com>
2026-02-22 15:02:30 +0100

    Create Paper7.m

---
+++ _publications/Paper7.m
@@ -0,0 +1,13 @@
+---
+title: "C6H6 condensation on Titan’s stratospheric aerosols: An integrated laboratory, modeling and experimental approach"
+collection: publications
+category: manuscripts
+permalink: /publication/2010-10-01-paper-title-number-2
+date: 2020-01-01
+venue: 'Proceedings of the IAU'
+paperurl: 'https://daviddubois5.github.io/daviddubois/files/c6h6-condensation-on-titans-stratospheric-aerosols-an-integrated-laboratory-modeling-and-experimental-approach.pdf'
+bibtexurl: 'https://daviddubois5.github.io/daviddubois/files/CambridgeCore_Citation_22Feb2026.txt'
+citation: 'Dubois et al. (2020). Proceedings of the IAU 350 5-8'
+---
+
+Saturn's moon Titan was explored by the Cassini mission for nearly 13 years. Important discoveries made during the Cassini mission include the observations of stratospheric clouds in Titan's cold polar regions in which spectral features or organic molecules were detected in the infrared (<100 um). In particular, benzene (C6H6) ice spectral signatures were recently detected at unexpectedly high altitudes over the South Pole. The combined experimental, modeling and observational effort presented here has been devised and executed in order to interpret these high altitude benzene observations. Our multi-disciplinary approach aims to understand and characterize the microphysics of benzene clouds in Titan's South Pole.
